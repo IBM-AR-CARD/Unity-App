1378581d22dcf1a380cbcfee1ce096d7362b27e6
Henry Zhang <henryzhang9802@gmail.com>
2019-12-15 14:09:32 +0000

    Can now change unity text mesh

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainScript : MonoBehaviour
 {
+    public TextMesh mytext = null;
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("started");
+        mytext.text="hello world!";
         
     }
 
