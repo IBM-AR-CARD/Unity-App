05110ae1bb895da12a591ee25e5e3dfe4fc5f60e
Henry Zhang <henryzhang9802@gmail.com>
2019-12-21 00:55:48 +0000

    Add more characters

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -11,7 +11,7 @@ public class MainScript : MonoBehaviour, IEventSystemHandler
     public TextMesh mytext = null;
     public GameObject player = null;
     private String currentAnimation = "idle";
-    private String[] characterList = new string[]{ "TestMale", "Luffy" };
+    private String[] characterList = new string[]{ "TestMale", "Luffy", "FitFemale", "Jiraiya", "YodaRigged" };
 
     // Start is called before the first frame update
     void Start()
