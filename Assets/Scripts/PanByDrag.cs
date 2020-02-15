fd3319061c95f2767c94b9fe26b9415d7f6d953c
Henry Zhang <henryzhang9802@gmail.com>
2020-02-15 22:03:32 +0000

    Add camera pan

---
+++ Assets/Scripts/PanByDrag.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanByDrag : MonoBehaviour
+{
+    public float speed = 0.1F;
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+        }
+
+        float X;
+        float Y;
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed * 10, -Input.GetAxis("Mouse X") * speed * 10, 0));
+            X = transform.rotation.eulerAngles.x;
+            Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(X, Y, 0);
+        }
+    }
+}
